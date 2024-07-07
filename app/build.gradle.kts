2e90cf78a6c0ccbc538eb8bd8cefd9b756d39dd4
varunkataria <vkataria35@gmail.com>
2024-07-06 20:53:21 -0700

    Update hilt version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
@@ -74,7 +74,6 @@ dependencies {
     ksp(libs.hilt.android.compiler)
     // Room
     implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
 
     // optional - Kotlin Extensions and Coroutines support for Room
