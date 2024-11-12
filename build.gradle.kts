19bfb24a4a3f584ef7967d9e333de173f827ca22
varunkataria <vkataria35@gmail.com>
2024-11-11 18:18:02 -0800

    Complete Kotlin version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20"
+    id("com.google.devtools.ksp") version "2.0.21-1.0.25"
 }
